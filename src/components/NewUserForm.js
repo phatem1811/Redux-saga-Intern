3d09735e05c73cf17bcae33b23aadc0b0f69b6ca
phatem1811 <21110739@student.hcmute.edu.vn>
2024-08-25 22:19:54 +0700

    convert to ant design

--- src/components/NewUserForm.js
+++ src/components/NewUserForm.js
@@ -1,60 +1,75 @@
 import React, { Component } from 'react';
-import { Button, Form, FormGroup, Label, Input } from 'reactstrap';
+import { Button, Form, Input, notification } from 'antd';
 
 class NewUserForm extends Component {
-
     state = {
         firstName: '',
         lastName: ''
     };
 
-    handleSubmit = e => {
-        e.preventDefault();
-        const { firstName, lastName } = this.state;
+    handleSubmit = values => {
+        const { firstName, lastName } = values;
 
+        // Trigger the onSubmit prop function
         this.props.onSubmit({
             firstName,
             lastName
         });
 
+        // Reset the form state
         this.setState({
             firstName: '',
             lastName: ''
         });
-    };
 
-    handleFirstNameChange = e => {
-        this.setState({
-            firstName: e.currentTarget.value
-        });
-    };
-
-    handleLastNameChange = e => {
-        this.setState({
-            lastName: e.currentTarget.value
+        // Show success notification
+        notification.success({
+            message: ' Created Success',
+            description: ` ${firstName} ${lastName} has been successfully created.`,
+            placement: 'topRight',
         });
     };
 
     render() {
         return (
-            <Form onSubmit={this.handleSubmit}>
-                <FormGroup>
-                    <Label>
-                        First name
-                    </Label>
-                    <Input required type="text" value={this.state.firstName} onChange={this.handleFirstNameChange} />
-                </FormGroup>
-                <FormGroup>
-                    <Label>
-                        Last name
-                    </Label>
-                    <Input required type="text" value={this.state.lastName} onChange={this.handleLastNameChange} />
-                </FormGroup>
-                <FormGroup>
-                    <Button block outline type="submit" color="primary">
+            <Form
+                onFinish={this.handleSubmit}
+                name="wrap"
+                labelCol={{ flex: '110px' }}
+                labelAlign="left"
+                labelWrap
+                wrapperCol={{ flex: 1 }}
+                colon={false}
+                style={{ maxWidth: 600 }}
+                initialValues={this.state}
+            >
+                <Form.Item
+                    label="First Name"
+                    name="firstName"
+                    rules={[{ required: true, message: 'Please input your first name!' }]}
+                >
+                    <Input
+                        value={this.state.firstName}
+                        onChange={e => this.setState({ firstName: e.target.value })}
+                    />
+                </Form.Item>
+
+                <Form.Item
+                    label="Last Name"
+                    name="lastName"
+                    rules={[{ required: true, message: 'Please input your last name!' }]}
+                >
+                    <Input
+                        value={this.state.lastName}
+                        onChange={e => this.setState({ lastName: e.target.value })}
+                    />
+                </Form.Item>
+
+                <Form.Item label=" ">
+                    <Button type="primary" htmlType="submit" block>
                         Create
                     </Button>
-                </FormGroup>
+                </Form.Item>
             </Form>
         );
     }
